fdf42179c728bd3473610dbd53e68f88e0071e66
rubayatsaikat <saikat091992@gmail.com>
2022-09-28 16:48:59 +0600

    new change

--- SoftwareTesting/src/SoftwareTesting.java
+++ SoftwareTesting/src/SoftwareTesting.java
@@ -1,9 +1,13 @@
-
 public class SoftwareTesting {
 
-	public static void main(String[] args) {
+	public static void main(String[]args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Software Testing");
+		// Test Print
+		//System.out.println("Hello Software Testing");
+		// For command line Print
+		//System.out.println(args[0]+ " " +args[1]);
+		System.out.println("Software" + " " + "Testing");
+		//System.out.println("Hello" + " " + "World");
 	}
 
 }
