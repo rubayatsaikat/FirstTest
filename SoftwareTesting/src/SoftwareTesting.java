b946a00435258274aa0af560e75ad90970eb411b
rubayatsaikat <saikat091992@gmail.com>
2022-09-30 11:52:45 +0600

    test

--- SoftwareTesting/src/SoftwareTesting.java
+++ SoftwareTesting/src/SoftwareTesting.java
@@ -1,13 +1,15 @@
-public class SoftwareTesting {
+package javaProgram;
+import javaProgram.Add;
+//import static javaProgram.Add.add;
 
-	public static void main(String[]args) {
+public class SoftwareTesting {
+	static int sum;
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// Test Print
-		//System.out.println("Hello Software Testing");
-		// For command line Print
-		//System.out.println(args[0]+ " " +args[1]);
-		System.out.println("Software" + " " + "Testing");
-		//System.out.println("Hello" + " " + "World");
+		Add a = new Add();
+		sum = a.add(5, 3);
+		System.out.println(sum);
+		
 	}
 
 }
